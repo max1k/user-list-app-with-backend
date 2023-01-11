7566959e709e7f7a4ae7b2e4b43dc97e709475a2
Maxim Kvasnikov <maxkvasnikov@gmail.com>
2023-01-11 18:52:31 +0300

    added configurable response delays

--- backend/src/main/kotlin/ru/mxk/person_backend/controller/PersonController.kt
+++ backend/src/main/kotlin/ru/mxk/person_backend/controller/PersonController.kt
@@ -1,38 +1,52 @@
 package ru.mxk.person_backend.controller
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 import ru.mxk.person_backend.model.Person
 import ru.mxk.person_backend.service.api.PersonService
+import java.time.Duration
 import java.util.*
 
 @RestController
 @RequestMapping("/person")
-class PersonController(private val personService: PersonService) {
+class PersonController(
+    private val personService: PersonService,
+    @Value("#{T(java.time.Duration).parse('\${app.response.delay}')}") private val responseDelay: Duration
+) {
     private val logger = LoggerFactory.getLogger(PersonController::class.java)
 
     @PostMapping
     fun save(@RequestBody person: Person): Person {
-        logger.info("save request. id=${person.id}")
+        preprocess("save request. id=${person.id}")
         return personService.save(person)
     }
 
     @GetMapping("/all")
     fun getAll(): List<Person> {
-        logger.info("getAll request")
+        preprocess("getAll request")
         return personService.getAll()
     }
 
     @GetMapping("/{personId}")
     fun getById(@PathVariable personId: UUID): Person {
-        logger.info("findById request. id=$personId")
+        preprocess("findById request. id=$personId")
         return personService.findById(personId)
     }
 
     @DeleteMapping("/{personId}")
     fun delete(@PathVariable personId: UUID): Person {
-        logger.info("remove request. id=$personId")
+        preprocess("remove request. id=$personId")
         return personService.remove(personId)
+    }
+
+    private fun preprocess(description: String) {
+        val logString = if (responseDelay.isZero) description else "$description. Delaying $responseDelay"
+        logger.info(logString)
+
+        if (!responseDelay.isZero) {
+            Thread.sleep(responseDelay.toMillis())
+        }
     }
 
 }
--- backend/src/main/kotlin/ru/mxk/person_backend/service/impl/PersonServiceImpl.kt
+++ backend/src/main/kotlin/ru/mxk/person_backend/service/impl/PersonServiceImpl.kt
@@ -1,36 +1,23 @@
 package ru.mxk.person_backend.service.impl
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import ru.mxk.person_backend.model.Person
 import ru.mxk.person_backend.repository.PersonRepository
 import ru.mxk.person_backend.service.api.PersonService
-import java.time.Duration
 import java.util.*
 
 @Service
 class PersonServiceImpl(
-    private val personRepository: PersonRepository,
-    @Value("#{T(java.time.Duration).parse('\${app.response.delay}')}") private val responseDelay: Duration
+    private val personRepository: PersonRepository
 ) : PersonService {
 
-    override fun getAll(): List<Person> {
-        Thread.sleep(responseDelay.toMillis())
-        return personRepository.findAll().toList()
-    }
+    override fun getAll(): List<Person> = personRepository.findAll().toList()
 
-    override fun findById(personId: UUID): Person {
-        Thread.sleep(responseDelay.toMillis())
-        return personRepository.findById(personId).orElseThrow()
-    }
+    override fun findById(personId: UUID): Person = personRepository.findById(personId).orElseThrow()
 
-    override fun save(person: Person): Person {
-        Thread.sleep(responseDelay.toMillis())
-        return personRepository.save(person)
-    }
+    override fun save(person: Person): Person = personRepository.save(person)
 
     override fun remove(personId: UUID): Person {
-        Thread.sleep(responseDelay.toMillis())
         val person = findById(personId)
         personRepository.delete(person)
         return person
